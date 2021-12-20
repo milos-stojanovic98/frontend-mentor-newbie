760928b492f51b1ff4b251e104018af1fbfa9377
Milos Stojanovic <milos.stojanovic@wearelion.dev>
2021-12-20 20:31:49 +0100

    javascript added

--- faq-accordion-card/script.js
+++ faq-accordion-card/script.js
@@ -1,1 +1,14 @@
-const 
+const button = document.getElementById("buttonn");
+const paragraph = document.getElementById("paragraphh");
+const second = document.getElementById("second");
+
+button.addEventListener("click", () => toggle())
+
+const toggle = () => {
+    paragraph.classList.toggle("show-paragraph");
+    if(paragraph.classList.contains("show-paragraph")){
+        second.style.fontWeight = 900;
+    }else{
+        second.style.fontWeight = 400;
+    } 
+}
