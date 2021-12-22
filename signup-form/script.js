0f792e47a1eb4904eed4bc0fdb23b70399ccfc33
Milos Stojanovic <milos.stojanovic@wearelion.dev>
2021-12-22 17:38:50 +0100

    error icons added

--- signup-form/script.js
+++ signup-form/script.js
@@ -13,27 +13,40 @@ const successful = document.getElementById("successful");
 
 submitBtn.addEventListener("click", () => {
         if (firstName.value === "") {
+            firstName.style.background = "url(./images/icon-error.svg) no-repeat right";
             fnError.innerHTML = "First name cannot be empty";
             setTimeout(() => {
                 fnError.innerHTML = "";
+                firstName.style.background = "none";
             }, 1500);
 
         } if (lastName.value === "") {
+            lastName.style.background = "url(./images/icon-error.svg) no-repeat right";
             lnError.innerHTML = "Last Name cannot be empty";
             setTimeout(() => {
                 lnError.innerHTML = "";
+                lastName.style.background = "none";
             }, 1500);
 
         } if (email.value === "") {
-            emailError.innerHTML = "email@example/com";
+            email.style.background = "url(./images/icon-error.svg) no-repeat right";
+            email.value = "email@example/com"
+            email.style.color = "hsl(0, 100%, 74%)";
+            emailError.innerHTML = "Looks like this isn't and email";
             setTimeout(() => {
                 emailError.innerHTML = "";
+                email.style.background = "none";
+                email.value = "";
+                email.style.color = "black";
             }, 1500);
 
         } if (password.value === "") {
+            password.style.background = "url(./images/icon-error.svg) no-repeat right";
             passwordError.innerHTML = "Password cannot be empty";
             setTimeout(() => {
                 passwordError.innerHTML = "";
+                password.style.background = "none";
+                password.value = "";
             }, 1500);
 
         } else {
